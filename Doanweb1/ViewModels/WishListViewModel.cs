11f9baa53ca5cd6f7a328bf65cac8c2269cc1f0f
Trang Do <trangdo.contact@gmail.com>
2023-03-30 03:53:15 +0700

    feature: wish list

---
+++ Doanweb1/ViewModels/WishListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Doanweb1.ViewModels
+{
+    public class WishListViewModel
+    {
+        public int ProductId { get; set; }
+        public DateTime AddedAt { get; set; } = DateTime.Now;
+    }
+}
